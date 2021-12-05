f91e28648b0153c3a98871f994999b7e01e320aa
Lasse Haslund <72796767+Tofthaslund@users.noreply.github.com>
2021-12-05 08:43:49 +0000

    finised counter app js

--- 100js/counter/counter.js
+++ 100js/counter/counter.js
@@ -1,21 +1,40 @@
-const count = document.querySelector('.count');
-// const add = document.querySelector('.add');
-// const reset = document.querySelector('.reset');
-// const sub = document.querySelector('.subtract');
-const buttons = document.querySelectorAll('.buttons');
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const sub = document.querySelector(".subtract");
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
 
-buttons.addEventListener('click', function(e) {
-    if (e.target.classList.contains('add')) {
-        console.log('add');
-    }
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    count.innerHTML++;
+    setColor();
+  }
+  if (e.target.classList.contains("subtract")) {
+    count.innerHTML--;
+    setColor();
+  }
+  if (e.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+    setColor();
+  }
 });
 
-// add.addEventListener('click', () => {
-//     count.innerHTML++;
+function setColor() {
+  if (count.innerHTML > 0) {
+    count.style.color = "yellow";
+  } else if (count.innerHTML < 0) {
+    count.style.color = "orangered";
+  } else {
+    count.style.color = "#fff";
+  }
+}
+
+// add.addEventListener("click", () => {
+//   count.innerHTML++;
 // });
-// sub.addEventListener('click', () => {
-//     count.innerHTML--;
+// sub.addEventListener("click", () => {
+//   count.innerHTML--;
 // });
-// reset.addEventListener('click', () => {
-//     count.innerHTML = 0;
+// resetCount.addEventListener("click", () => {
+//   count.innerHTML = 0;
 // });
