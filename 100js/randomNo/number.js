cccddb4d4a0d80df71f0e8300625fb9706b23069
Lasse Haslund <72796767+Tofthaslund@users.noreply.github.com>
2021-12-06 16:10:12 +0000

    added JS for random number generater

--- 100js/randomNo/number.js
+++ 100js/randomNo/number.js
@@ -1,2 +1,13 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
 
-const rand = Math.random();
+const generateNumber = () => {
+  // Generate number btn 1 and 10
+  const randomNumber = Math.floor(Math.random() * 10000 + 1);
+  number.innerHTML = randomNumber;
+};
+
+btn.addEventListener("click", generateNumber);
+
+generateNumber();
+
